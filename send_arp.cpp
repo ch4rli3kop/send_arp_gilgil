ad9f3bf2f005b599be253c3f77f365fc2216c6c6
ch4rli3kop <pch2180@gmail,com>
2019-08-02 09:49:29 -0700

    modify

--- send_arp.cpp
+++ send_arp.cpp
@@ -21,7 +21,7 @@ struct arp_header{
     uint16_t hd_type;
     uint16_t pr_type;
     uint8_t hd_len;
-    uint8_t pd_len;
+    uint8_t pr_len;
     uint16_t opcode;
     uint8_t smac[6];
     uint8_t sip[4];
@@ -40,8 +40,8 @@ bool check_arp(const u_char* packet){
 }
 
 void get_mac(uint8_t* smac, char* interface) {
-    #define HWADDR_len 6
-    int s,i;
+
+    int s;
     struct ifreq ifr;
     unsigned char* test;
 
@@ -69,11 +69,40 @@ int main(int argc, char* argv[]){
       return -1;
     }
 
+    uint8_t sip[4];
+    uint8_t tip[4];
+
+    inet_aton(argv[2], (in_addr*)sip);
+    inet_aton(argv[3], (in_addr*)tip);
+
+
     struct ethernet_header eth;
     struct arp_header arp;
 
-    memcpy(eth.dst_mac, "\xff\xff\xff\xff\xff\xff", 6);
+    // make ethernet
+    printf(argv[0]);
     get_mac(&eth.src_mac[0], argv[0]);
+
+    printf("AAA");
+    memcpy(eth.dst_mac, "\xff\xff\xff\xff\xff\xff", 6);
+    memcpy(eth.type, "\x08\x06", 2);
+    // make arp
+    memcpy((void*)arp.hd_type, "\x00\x01", 2);
+    memcpy((void*)arp.pr_type, "\x08\x00", 2);
+    arp.hd_len = (uint8_t)'\x06';
+    arp.pr_len = (uint8_t)'\x04';
+    memcpy((void*)arp.opcode, "\x00\x01", 2);
+    memcpy(arp.smac, eth.src_mac, 6);
+    memcpy(arp.sip, sip, 4);
+    memcpy((void*)arp.tmac, "\x00\x00\x00\x00\x00\x00", 6);
+    memcpy(arp.tip, tip, 4);
+
+    uint8_t buf[42];
+    memcpy(buf, &eth, 14);
+    memcpy(&buf[14], &arp, 28);
+
+    pcap_sendpacket(handle, buf, 42);
+    printf("AAAA");
 
     while (true) {
       struct pcap_pkthdr* header;
@@ -86,11 +115,12 @@ int main(int argc, char* argv[]){
       if(check_arp(packet)) break;
     }
 
+
+    // extract target mac address
+    // make packet
+    // pcap_sendpacket(handle, packet, )
+
     pcap_close(handle);
-
-
-
-
 
     return 0;
 }
